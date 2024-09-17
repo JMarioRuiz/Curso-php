a72707794592a6025993abd676f904daac72b8a5
Mario Ruiz <josemario.bros@hotmail.com>
2024-09-16 18:48:25 -0700

    conceptos base de datos

---
+++ baseDeDatos.php
@@ -0,0 +1,12 @@
+DBMS
+son los gestores de base de datos
+
+existen dos tipos de dbms, no sql y sql
+las sql son mas estructuradas que las no sql
+
+las api's sirver para comunicarnos entre el servidor php y la base de datos
+existen dos apis para comunicarnos, PDO y MySQLi
+
+MySQLi es una api que sirve solamnete para DBMS de MySQL y DBO se puede utilzar
+para varios gestores de base de datos
+
