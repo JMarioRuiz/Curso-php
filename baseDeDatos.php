a1761c471dc5b58f97c3ad7503e51eed741e4125
Mario Ruiz <josemario.bros@hotmail.com>
2024-09-17 18:20:25 -0700

    CRUD base de datos

--- baseDeDatos.php
+++ baseDeDatos.php
@@ -1,4 +1,4 @@
-DBMS
+<!-- DBMS
 son los gestores de base de datos
 
 existen dos tipos de dbms, no sql y sql
@@ -8,5 +8,10 @@ las api's sirver para comunicarnos entre el servidor php y la base de datos
 existen dos apis para comunicarnos, PDO y MySQLi
 
 MySQLi es una api que sirve solamnete para DBMS de MySQL y DBO se puede utilzar
-para varios gestores de base de datos
+para varios gestores de base de datos 
 
+-->
+
+<?php 
+print_r(PDO::getAvailableDrivers());
+?>ge
